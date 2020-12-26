3e9cc7cd585cd68bc7b796e27a9eeb702607561f
Felix Wiegand <fxwiegand@gmail.com>
2020-12-26 21:14:55 +0100

    First try using what the fridge contains

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use log::warn;
+use regex::Regex;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::env;
@@ -37,6 +38,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .filter(|t| data.to_lowercase().contains(*t))
                     .collect();
 
+                let fridge_contains = Regex::new(r"^*contains\s(\s*(and)?[a-z]*,?)*.*$").unwrap();
+
                 let answer = if data.to_lowercase().contains("recipe")
                     && data.to_lowercase().contains("random")
                 {
@@ -56,24 +59,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     {
                         Ok(resp) => {
                             if !resp.get("recipes").unwrap().as_array().unwrap().is_empty() {
-                                let dish = resp.get("recipes").unwrap()[0]
-                                    .get("title")
-                                    .unwrap()
-                                    .to_string();
-                                let summary = strip::strip_tags(
-                                    &resp.get("recipes").unwrap()[0]
-                                        .get("summary")
-                                        .unwrap()
-                                        .to_string()
-                                        .trim_start_matches('\"')
-                                        .trim_end_matches('\"'),
-                                );
-                                let source_url = resp.get("recipes").unwrap()[0]
-                                    .get("sourceUrl")
-                                    .unwrap()
-                                    .to_string();
+                                let resp_content =
+                                    obtain_information(resp.as_array().unwrap().to_vec());
 
-                                format!("What about some {} today. {} You can find the full recipe here: {}.", dish.trim_start_matches('\"').trim_end_matches('\"'), summary, source_url.trim_start_matches('\"').trim_end_matches('\"'))
+                                format!("What about some {} today. {} You can find the full recipe here: {}.", resp_content.get("dish").unwrap(), resp_content.get("summary").unwrap(), resp_content.get("source_url").unwrap())
                             } else {
                                 "I couldn't find any recipe based on your given tags. Maybe ask for a more general recipe.".to_string()
                             }
@@ -83,8 +72,75 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             "It seems like something went wrong. I am really sorry.".to_string()
                         }
                     }
+                } else if fridge_contains.is_match(&data.to_lowercase()) {
+                    let matches = fridge_contains
+                        .find(&data.to_lowercase())
+                        .unwrap()
+                        .as_str()
+                        .to_string();
+                    let items: Vec<_> = matches
+                        .trim_start_matches("contains ")
+                        .split_whitespace()
+                        .map(|item| item.trim_matches(','))
+                        .filter(|item| item != &"and")
+                        .collect();
+
+                    let client = reqwest::Client::new();
+                    let mut query = HashMap::new();
+                    query.insert("ingredients", items.join(","));
+                    query.insert("number", "1".to_string());
+
+                    match client
+                        .get("https://api.spoonacular.com/recipes/findByIngredients")
+                        .query(&[("apiKey", api_key.clone())])
+                        .query(&query)
+                        .send()
+                        .await?
+                        .json::<Value>()
+                        .await
+                    {
+                        Ok(resp) => {
+                            if !resp.as_array().unwrap().is_empty() {
+                                let id = resp.as_array().unwrap()[0]
+                                    .get("id")
+                                    .unwrap()
+                                    .as_i64()
+                                    .unwrap();
+
+                                match client
+                                    .get(
+                                        &(String::from("https://api.spoonacular.com/recipes/")
+                                            + &id.to_string()
+                                            + "/information"),
+                                    )
+                                    .query(&[("apiKey", api_key.clone())])
+                                    .send()
+                                    .await?
+                                    .json::<Value>()
+                                    .await
+                                {
+                                    Ok(s_res) => {
+                                        let resp_content = obtain_information(vec![s_res]);
+
+                                        format!("You could make some {} today. {} The full recipe is right here: {}.", resp_content.get("dish").unwrap(), resp_content.get("summary").unwrap(), resp_content.get("source_url").unwrap())
+                                    }
+                                    Err(e) => {
+                                        warn!("{}", e.to_string());
+                                        "It seems like something went wrong. I am really sorry."
+                                            .to_string()
+                                    }
+                                }
+                            } else {
+                                "I couldn't find any recipe based on your given ingredients. Maybe ask with less ingredients or just ask for a random recipe.".to_string()
+                            }
+                        }
+                        Err(e) => {
+                            warn!("{}", e.to_string());
+                            "It seems like something went wrong. I am really sorry.".to_string()
+                        }
+                    }
                 } else {
-                    "I can not quite understand you. Try asking me for a random recipe.".to_string()
+                    "I can not quite understand you. Try asking me for a random recipe or tell me whats left in your fridge.".to_string()
                 };
 
                 // Answer message with recipe.
@@ -94,4 +150,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     Ok(())
+}
+
+fn obtain_information(resp: Vec<Value>) -> HashMap<String, String> {
+    let mut result = HashMap::new();
+    let mut dish = resp[0].get("title").unwrap().to_string();
+    dish = dish.trim_matches('\"').to_string();
+    result.insert("dish".to_string(), dish);
+    let summary = strip::strip_tags(
+        &resp[0]
+            .get("summary")
+            .unwrap()
+            .to_string()
+            .trim_matches('\"'),
+    );
+    result.insert("summary".to_string(), summary);
+    let mut source_url = resp[0].get("sourceUrl").unwrap().to_string();
+    source_url = source_url.trim_matches('\"').to_string();
+    result.insert("source_url".to_string(), source_url);
+
+    result
 }
